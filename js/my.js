a8fd36ea4f5e0b113174fdc8f4934f7ce4ef6301
devdariannianni <devdariannianni@gmail.com>
2022-05-13 22:37:59 +0400

    my first commit

---
+++ js/my.js
@@ -0,0 +1,63 @@
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 3000);
+}
+
+
+
+
+function aniFunction() {
+    var x = document.getElementById('login')
+    if (x.style.display === 'none') {
+        x.style.display = 'block';}
+        else {
+            x.style.display = 'none'
+        }
+}
+
+
+//  readmore
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
+
+function myFunction() {
+  var dots = document.getElementById("main");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("moreBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
+
+
+// burger menu
+function menuFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
